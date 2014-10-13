1e9cf4532190d47cff4dcb6154065ca35e8b5dfd
KokaKiwi <kokakiwi@kokakiwi.net>
2014-10-13 14:11:30 +0200

    fu, why some linux distribs doesn't have pip? :(

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ PREFIX=${PREFIX:-/usr}
 bindir=${PREFIX}/bin
 
 echo "Installing dependencies..."
+easy_install pip >/dev/null 2>&1
 pip install -r requirements.txt > /dev/null 2>&1
 
 echo "Installing tools..."
